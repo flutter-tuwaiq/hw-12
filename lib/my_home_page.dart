b5c79e9c3b7def2e480a4530ddebb1c50d97629c
ZiyadALharbi <ziyad.swengineer@gmail.com>
2023-05-29 00:40:53 +0300

    hw-12

---
+++ lib/my_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Text("Ziyad Alharbi"),
+      )),
+    );
+  }
+}
